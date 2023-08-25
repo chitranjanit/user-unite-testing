c5b4a1a60f21e4dc693e6e6409ed7d6ca811f918
chitranjan mukhiya <chitranjan.it@gmail.com>
2023-08-25 06:01:35 +0530

    unite testing data

--- src/UserFrom.test.js
+++ src/UserFrom.test.js
@@ -49,3 +49,30 @@ test('it calls unUserAdd when the form is submitted', () => {
   expect(mock).toHaveBeenCalled();
   expect(mock).toHaveBeenCalledWith({ name: 'chitra', email: 'chitra@gmail.com' });
 });
+
+test('empties the two inputs when form is submitted', () => {
+  // render the component
+  render(<UserForm onUserAdd={() => {}} />);
+
+  // select the inputbox
+  const nameInput = screen.getByRole('textbox', {
+    name: /name/i,
+  });
+  const emailInput = screen.getByRole('textbox', {
+    name: /email/i,
+  });
+  const button = screen.getByRole('button');
+
+  // type the input data
+  user.click(nameInput);
+  user.keyboard('chitra');
+  user.click(emailInput);
+  user.keyboard('chitra@gmail.com');
+
+  // click on add button
+  user.click(button);
+
+  // click expect the value
+  expect(nameInput).toHaveValue('');
+  expect(emailInput).toHaveValue('');
+});
